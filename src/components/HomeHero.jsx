86308474e207808754664334aa47e58537c656d4
shohjaxon09092005 <qodiraliyevshohjaxon2005@gmail.com>
2024-08-23 10:23:37 +0500

    first commit

---
+++ src/components/HomeHero.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../style/homeHero.css'
+import HeroForm from './HeroForm'
+import { useLoaderData, useLocation } from 'react-router-dom'
+function HomeHero() {
+  
+  return (
+    <section className='homeHero'>
+      
+        <div className="overlay">
+
+        </div>
+      <div className="container">
+        <div className="header__content">
+           <h2>Helping Others</h2>
+        <h1>Live & Travel</h1>
+        <h4>Special offers to suit your plan</h4>  
+        </div>
+       <HeroForm/>
+      </div>
+    </section>
+  )
+}
+
+export default HomeHero
---
+++ src/page/HomePage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import HomeHero from '../components/HomeHero'
+import HeroForm from '../components/HeroForm'
+import Capital from '../components/Capital'
+import Banner from '../components/Banner'
+import Review from '../components/Review'
+
+function HomePage() {
+  return (
+    <main>
+      <HomeHero/>
+      <Capital/>
+      <Banner/>
+      <Review/>
+     
+    </main>
+  )
+}
+
+export default HomePage
